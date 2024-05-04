e7104f7a4acadee5b018e0dac5e828492ff36868
Frank Miguel Rondon Rodriguez <fmrondon14@gmail.com>
2024-05-04 10:47:04 -0400

    uhbgjfhgdrftghjuhygtfryghugtrfgyhgtrftgyhgftdreswdrftyghugtrfdewsdrftyghugtrfdewsedrftyghtrfdewsedrftygrfdewsdrftygrfdewsdrfvtgybtdrecfvtgbyufvtdrcxesctfvtygbtfrdesdrxcgfvbyfvtdresfvtgbytfrdesrftgyrfde4rftygrfde4rftygbfrdertygbuhnygtrfdygbhnuygtfrderftgybhnubugfvtdcresxrcfgvbfvdrterfvgbyvfrdescfvgbhnbgfdscfvgbhn

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import getPokemonOptions from './pokemonApi/getPokemonApi';
-
+import getPokemonOptions from './pokemonApi/getPokemonApi'
 
 getPokemonOptions()
 </script>
 
 <template>
-    <p class="text-9xl">hola</p>
+  <p class="text-9xl">hola</p>
 </template>
-
-
--- src/pokemonApi/pokemonApi.ts
+++ src/pokemonApi/pokemonApi.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const pokemonApi= axios.create({
-    baseURL: "https://pokeapi.co/api/v2/pokemon"
+export const pokemonApi = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/pokemon'
 })
